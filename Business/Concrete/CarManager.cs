829f5bdfa60bb606fc1503663b0863299a9918a7
Serkan Aydın <aydinserkan2004@gmail.com>
2022-08-28 00:20:12 +0300

    28.08.2022 Araba Kirama Projesi

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        // Bu komut ile ister Memory de ister EntityFramework ile çalışabilirsin
+        ICarDal _carDal;
+
+        // bu constractor ile hangi veritabanı olursa olsun çalışır. Sadece istediğimiz veritabanını seçmemiz gerekiyor
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
